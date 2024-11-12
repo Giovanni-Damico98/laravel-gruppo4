250f1581f2791aab1986407534fddf06fe559be4
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-12 16:39:55 +0100

    fixed PostController create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,8 +22,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        $posts = Post::all();
-        return view("admin.posts.create", compact("post"));
+        $posts = new Post();
+        return view("admin.post.create", compact("post"));
     }
 
     /**
