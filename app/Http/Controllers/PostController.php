6dfc1ed1c45cfdc68e1a3cea76ff108aa74713b2
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-12 15:18:29 +0100

    controller  function destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -62,6 +62,8 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route("posts.index");
     }
 }
